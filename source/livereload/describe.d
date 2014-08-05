c0c8d81b17de841bc9b210474b615d7243c93d0c
Alan Potteiger <alanpotteiger@gmail.com>
2014-08-05 02:54:19 -0400

    removed temporary function

--- source/livereload/describe.d
+++ source/livereload/describe.d
@@ -9,10 +9,9 @@ import vibe.data.json;
 struct Dependency {
 	string[] copyFiles, libs, importPaths, files, versions;
 }
-Dependency[] getDependencyData() {
-	string describe = dubDescribe();
+Dependency[] getDependencyData(string dubDescription) {
 	Dependency[] dependencies;
-	Json json = parseJsonString(describe);
+	Json json = parseJsonString(dubDescription);
 
 	json = json["packages"];
 
@@ -33,17 +32,10 @@ Dependency[] getDependencyData() {
 
 	return dependencies;
 }
-unittest {
-	writeln("Dependencies:\n", getDependencyData());
-}
 string[] getArrayContents(Json json, string value) {
 	string[] contents;
 	foreach (size_t i, Json v; json[value]) {
 		contents ~= v.get!string;
 	}
 	return contents;
-}
-string dubDescribe() {
-	auto dub = execute(["dub", "describe"]);
-	return dub.output;
 }
