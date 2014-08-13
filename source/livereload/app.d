2492e0c09ab8c7610786416429492b03f4c7a186
rikki cattermole <pbdadmin@gmail.com>
2014-08-13 22:53:48 +1200

    Clean up

--- source/livereload/app.d
+++ source/livereload/app.d
@@ -9,71 +9,89 @@ import std.path : buildPath;
 
 shared {
 	string pathToFiles;
-}
-
-void main(string[] args) {
-    version(Windows) {
-		if (environment.get("PWD", "") != "") {
-			// most likely e.g. cygwin *grumble*
-			auto value = execute(["cygpath", "-w", environment.get("PWD", "")]);
-			if (value.output[$-1] == '\n')
-				value.output.length--;
-			chdir(environment.get("CD", value.output));
-		} else {
-			chdir(environment.get("CD", getcwd()));
-		}
-	} else {
-		chdir(environment.get("PWD", environment.get("CD", getcwd())));
-	}
-	pathToFiles = getcwd();
     string configFile;
     string compiler = "dmd";
     string arch;
+}
 
+void main(string[] args) {
+    cwdUpdate();
+    getArgs();
+    if (testForCompiler())
+        return;
+    if (checkFiles())
+        return;
+
+	new LiveReload(pathToFiles, compiler, arch, configFile);
+
+	runEventLoop();
+}
+
+void cwdUpdate() {
+    version(Windows) {
+        if (environment.get("PWD", "") != "") {
+            // most likely e.g. cygwin *grumble*
+            auto value = execute(["cygpath", "-w", environment.get("PWD", "")]);
+            if (value.output[$-1] == '\n')
+                value.output.length--;
+            chdir(environment.get("CD", value.output));
+        } else {
+            chdir(environment.get("CD", getcwd()));
+        }
+    } else {
+        chdir(environment.get("PWD", environment.get("CD", getcwd())));
+    }
+}
+
+void getArgs() {
+    pathToFiles = getcwd();
+    
     version(X86) {
         arch = "x86";
     } else version(X86_64) {
         arch = "x86_64";
     }
+    
+    getOption("path", cast(string*)&pathToFiles, "Path of the files to operate on");
+    getOption("compiler", cast(string*)&compiler, "Compiler to use. Default: dmd");
+    getOption("config", cast(string*)&configFile, "Configuration file to use. Default: ${--path}/livereload.txt");
+    getOption("arch", cast(string*)&arch, "Architecture for the compiler to target. Default: x86 when built for 32bit and x86_64 for 64bit");
 
-	getOption("path", cast(string*)&pathToFiles, "Path of the files to operate on");
-	getOption("compiler", &compiler, "Compiler to use. Default: dmd");
-    getOption("config", &configFile, "Configuration file to use. Default: ${--path}/livereload.txt");
-    getOption("arch", &arch, "Architecture for the compiler to target. Default: x86 when built for 32bit and x86_64 for 64bit");
+    finalizeCommandLineOptions();
 
-	finalizeCommandLineOptions();
+    // this should equalize things a little bit
+    pathToFiles = Path(pathToFiles).toNativeString();
+}
 
-	// this should equalize things a little bit
-	pathToFiles = buildPath(pathToFiles);
+bool testForCompiler() {
+    if (testFor(compiler)) {
+    } else if (testFor("gdc")) {
+        compiler = "gdc";
+    } else if (testFor("ldc")) {
+        compiler = "ldc";
+    } else {
+        logError("No compiler on PATH variable");
+        return true;
+    }
+    
+    logInfo("Using compiler %s", compiler);
 
-	if (testFor(compiler)) {
-	} else if (testFor("gdc")) {
-		compiler = "gdc";
-	} else if (testFor("ldc")) {
-		compiler = "ldc";
-	} else {
-		logError("No compiler on PATH variable");
-		return;
-	}
+    if (!testFor("dub"))
+        logError("Dub is not on the PATH variable");
 
-	logInfo("Using compiler %s", compiler);
+    return false;
+}
 
-	if (!testFor("dub"))
-		logError("Dub is not on the PATH variable");
+bool checkFiles() {
+    if (configFile is null)
+        configFile = buildPath(pathToFiles, "livereload.txt");
+    
+    if (!(exists(configFile) && isFile(configFile))) {
+        logError("No valid configuration file found. Assumed %s", configFile);
+        return true;
+    }
 
-	if (configFile is null)
-		configFile = buildPath(pathToFiles, "livereload.txt");
-
-	if (!(exists(configFile) && isFile(configFile))) {
-		logError("No valid configuration file found. Assumed %s", configFile);
-		return;
-	}
-
-	new LiveReload(pathToFiles, compiler, arch, configFile);
-
-    //testfunc(compiler);
-
-	runEventLoop();
+    return false;
 }
 
 bool testFor(string app) {
@@ -83,52 +101,4 @@ bool testFor(string app) {
 	} catch(Exception e) {
 		return false;
 	}
-}
-
-void testfunc(string compI) {
-    import dub.dub;
-    import dub.project;
-    import dub.compilers.compiler;
-    import dub.package_;
-    import dub.generators.generator;
-    import dub.packagesupplier;
-
-    BuildSettings bs;
-    string[] debugVersions;
-    string[] registry_urls;
-
-    Compiler compiler = getCompiler(compI);
-    BuildPlatform bp = compiler.determinePlatform(bs, compI);
-    bs.addDebugVersions(debugVersions);
-
-    Dub vdub = new Dub();
-    vdub.loadPackageFromCwd();
-
-    auto subp = vdub.packageManager.getFirstPackage(vdub.projectName ~ ":static");
-
-    subp.info.buildSettings.sourceFiles[""] ~= "test.d";
-    subp.info.buildSettings.targetName = "static_test";
-
-    vdub.rootPath = subp.path;
-    vdub.loadPackage(subp);
-
-    logInfo("subpackage name %s", subp.name);
-
-    vdub.upgrade(UpgradeOptions.select);
-    vdub.upgrade(UpgradeOptions.upgrade|UpgradeOptions.printUpgradesOnly|UpgradeOptions.useCachedResult);
-    vdub.project.validate();
-
-    foreach(o; vdub.packageManager.completeSearchPath) {
-        logInfo(o.toNativeString());
-    }
-    
-    GeneratorSettings gensettings;
-    gensettings.platform = bp;
-    gensettings.config = vdub.project.getDefaultConfiguration(bp);
-    gensettings.compiler = compiler;
-    gensettings.buildType = "debug";
-
-    vdub.generateProject("build", gensettings);
-
-    vdub.shutdown();
 }
